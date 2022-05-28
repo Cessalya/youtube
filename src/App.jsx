e9a693d1fcd9624fb31a891fe4fb1d0d04028aa9
Cessalya <cessalyaputri090305@gmail.com>
2022-05-28 08:01:52 +0700

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import "./app.scss"
+import Home from "./page/Home";
+import Register from "./page/Register";
+import Watch from "./page/Watch";
+import Login from "./page/Login";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  // return <Home/>;
+  // return <Register/>;
+  return <Watch/>;
+  // return <Login/>;
 };
 
 export default App;
